55833122f27a563dd76cc3ad8539c854c5cc20eb
Gordon Chiang <gordonnnnnnnnnnn@gmail.com>
2023-06-06 00:06:43 -0700

    Add timer to frontend

--- backend/src/sportsbooks/playNow.ts
+++ backend/src/sportsbooks/playNow.ts
@@ -4,7 +4,7 @@ import { mkdirSync } from 'fs';
 export async function parsePage(page: Page): Promise<Record<string, string>> {
   const bettingLines: Record<string, string> = {};
 
-  await page.waitForNavigation({ waitUntil: 'networkidle0' }); 
+  await page.waitForNavigation({ waitUntil: 'networkidle0' });
   
   mkdirSync(`${__dirname}/../../lines/playNow/`, { recursive: true });
   await page.screenshot({ path: `${__dirname}/../../lines/playNow/all.png`, fullPage: true });
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,4 @@
-/* eslint-disable jsx-a11y/alt-text */
-import { useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import axios from 'axios';
 import './App.css';
 import { isValidHttpUrl } from './util/http';
@@ -11,9 +10,31 @@ export const App = () => {
   const [ playNowUrl, setPlayNowUrl ] = useState<string>('');
   const [ sportsinteractionUrl, setSportsinteractionUrl ] = useState<string>('');
 
+  const [ initialTimer, setInitialTimer ] = useState<number>(60);
+  const [ timer, setTimer ] = useState<number>(initialTimer);
+  const [ pauseTimer, setPauseTimer ] = useState<boolean>(false);
+
   const [ fanDuelBettingLines, setFanDuelBettingLines ] = useState<any>([]);
   const [ playNowBettingLines, setPlayNowBettingLines ] = useState<any>([]);
   const [ sportsInteractionBettingLines, setSportsInteractionBettingLines ] = useState<any>([]);
+
+  const reloadBettingLines = useCallback(async (): Promise<void> => {
+    console.log('reload')
+  }, []);
+
+  useEffect(() => {
+    if (pauseTimer) return;
+
+    const newTimer = timer-1;
+    const tick = setInterval(setTimer, 1000, newTimer === -1 ? initialTimer : newTimer);
+
+    const triggerReload = async () => {
+      await reloadBettingLines();
+    };
+    if (newTimer === -1) triggerReload();
+  
+    return () => clearInterval(tick);
+  }, [ initialTimer, pauseTimer, reloadBettingLines, timer ]);
 
   const loadBettingLines = async (type: BettingSite, url: string) => {
     if (!isValidHttpUrl(url)) {
@@ -22,33 +43,23 @@ export const App = () => {
     }
 
     try {
-      let resp;
-      let elements = [];
+      const resp = await axios.get('http://localhost:4200/scrape', { params: { type, url }});
+      const elements = Object.entries(resp.data).reduce<JSX.Element[]>((arr, [line, img]) => {
+        return line === 'all' ? arr : arr.concat(<img
+          id={ `${line}${img}` }
+          alt='betting-line'
+          src={ `http://localhost:4200/lines/${img}` }
+        />);
+      }, []);
+ 
       switch (type) {
         case 'fanDuel':
-          resp = await axios.get('http://localhost:4200/scrape', { params: { type, url }});
-          for (const [ line, img ] of Object.entries(resp.data)) {
-            if (line !== 'all') elements.push(<img src={ `http://localhost:4200/lines/${img}` }/>);
-          }
-          
           setFanDuelBettingLines(elements);
           break;
         case 'playNow':
-          resp = await axios.get('http://localhost:4200/scrape', { params: { type, url }});
-
-          for (const [ line, img ] of Object.entries(resp.data)) {
-            if (line !== 'all') elements.push(<img src={ `http://localhost:4200/lines/${img}` }/>);
-          }
-          
           setPlayNowBettingLines(elements);
           break;
         case 'sportsInteraction':
-          resp = await axios.get('http://localhost:4200/scrape', { params: { type, url }});
-
-          for (const [ line, img ] of Object.entries(resp.data)) {
-            if (line !== 'all') elements.push(<img src={ `http://localhost:4200/lines/${img}` }/>);
-          }
-          
           setSportsInteractionBettingLines(elements);
           break;
         default:
@@ -104,6 +115,31 @@ export const App = () => {
               loadBettingLines('sportsInteraction', sportsinteractionUrl);
             } }
           >Go</button>
+        </div>
+      </div>
+
+      <div className='timer'>
+        <div className='timer-input'>
+          <input
+            value={ initialTimer }
+            type='number'
+            onChange={ (e) => setInitialTimer(Number(e.target.value)) }
+          ></input>
+          <input
+            className='timer-view'
+            disabled={ pauseTimer }
+            readOnly={ true }
+            value={ timer }
+          ></input>
+          <button onClick={ () => setPauseTimer(!pauseTimer) }>
+            { `${pauseTimer ? 'Enable' : 'Disable' } Automatic Refresh` }
+          </button>
+          <button onClick={ async () => {
+            setTimer(initialTimer);
+            await reloadBettingLines();
+           } }>
+            Reload All Betting Lines
+          </button>
         </div>
       </div>
 
