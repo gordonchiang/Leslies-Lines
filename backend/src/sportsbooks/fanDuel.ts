4c3ee38d901fe6fc68802a7c1d3aee03f7c128d1
Gordon Chiang <gordonnnnnnnnnnn@gmail.com>
2023-06-11 03:08:44 -0700

    Fix fan duel webpage parsing

--- backend/src/sportsbooks/fanDuel.ts
+++ backend/src/sportsbooks/fanDuel.ts
@@ -1,11 +1,11 @@
-import { Page } from 'puppeteer';
+import { ElementHandle, Page } from 'puppeteer';
 import { mkdirSync } from 'fs';
 
 export async function parsePage(page: Page): Promise<Record<string, string>> {
   const bettingLines: Record<string, string> = {};
 
   try {
-    await page.waitForSelector('div.eventmarketgroup.marketgroup', { visible: true });
+    await page.waitForXPath('/html/body/div[1]/div/div/div/div[2]/div[2]/main/div/div[1]/div/div[2]/div[4]/ul', { visible: true });
   } catch {
     return bettingLines;
   }
@@ -14,32 +14,47 @@ export async function parsePage(page: Page): Promise<Record<string, string>> {
   await page.screenshot({ path: `${__dirname}/../../lines/fanDuel/all.png`, fullPage: true });
   bettingLines.all = 'fanDuel/all.png';
 
-  const elementHandles = await page.$$('div.eventmarketgroup.marketgroup div.event-market-header');
-
-  const gameLinesHandle = await page.$('div.markets-wrapper');
-  if (gameLinesHandle) {
-    const heading = await gameLinesHandle.$eval('span.coupontitle', element => element.textContent);
-    if (heading !== null) {
-      const path = `fanDuel/-1.png`;
-      await gameLinesHandle.screenshot({ path: `${__dirname}/../../lines/${path}` });
-  
-      bettingLines[heading] = path;
-    }
-  }
-
+  const elementHandles = await page.$$('#main > div > div > div > div > div > ul > li > div > div > div');
+  // const elementHandles = await page.$x('//*[@id="main"]/div/div[1]/div/div[2]/div[4]/ul/li/div/div/div');
   if (!elementHandles.length) return bettingLines;
 
   for (const [ i, handle ] of elementHandles.entries()) {
-    await handle.click();
-
-    const heading = await handle.$eval('h4.market-name-title', element => element.textContent);
+    const heading = await handle.$eval('span', element => {
+      const heading = element.textContent;
+      if (!heading?.includes('Game Lines')) element.click();
+      return heading;
+    });
     if (heading === null) continue;
-
+    await handle.$$eval('span', elements => {
+      elements.map(e => {
+        if (e.textContent?.includes('Show more')) e.click();
+      })
+    });
+ 
     const path = `fanDuel/${i}.png`;
-    await handle.screenshot({ path: `${__dirname}/../../lines/${path}` });
+    await handle.screenshot();
+    // await screenshotXPath(page, handle, path);
 
     bettingLines[heading] = path;
   }
 
   return bettingLines;
+}
+
+async function screenshotXPath(page: Page, elementHandle: ElementHandle<Node>, path: string): Promise<void> {
+  const rect = await page.evaluate(element => {
+    // @ts-ignore
+    const {x, y, width, height} = element.getBoundingClientRect();
+    return {left: x, top: y, width, height};
+  }, elementHandle);
+
+  await page.screenshot({
+    path: `${__dirname}/../../lines/${path}`,
+    clip: {
+      x: rect.left,
+      y: rect.top,
+      width: rect.width,
+      height: rect.height,
+    },
+  });
 }
